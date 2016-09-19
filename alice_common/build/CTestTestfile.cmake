2c6ff057efd7a40ca5a096fcd5b05c44f891c6c4
lubaogui <lbaogui@pku.edu.cn>
2016-09-19 14:21:23 +0800

    add new package

---
+++ alice_common/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/alice/alice_common/src
+# Build directory: /home/robot/alice/alice_common/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
