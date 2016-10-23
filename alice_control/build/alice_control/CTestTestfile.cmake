c2eb6a5264271d1835f274c0a067047d37eb855f
lubaogui <lbaogui@pku.edu.cn>
2016-10-24 00:09:41 +0800

    Update

---
+++ alice_control/build/alice_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/AliceRobot/alice_control/src/alice_control
+# Build directory: /home/robot/AliceRobot/alice_control/build/alice_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ alice_control/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/robot/AliceRobot/alice_control/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/robot/AliceRobot/alice_common/devel;/opt/ros/indigo".split(';') if "/home/robot/AliceRobot/alice_common/devel;/opt/ros/indigo" != "" else []
