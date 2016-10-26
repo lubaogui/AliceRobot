2371c50a211484d67b0a67fdb1d1707d9ad2e164
lubaogui <lbaogui@pku.edu.cn>
2016-10-26 15:49:29 +0800

    Update

---
+++ alice_common/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/AliceRobot/alice_common/src
+# Build directory: /home/robot/AliceRobot/alice_common/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(alice_description)
---
+++ alice_common/build/alice_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/AliceRobot/alice_common/src/alice_description
+# Build directory: /home/robot/AliceRobot/alice_common/build/alice_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
