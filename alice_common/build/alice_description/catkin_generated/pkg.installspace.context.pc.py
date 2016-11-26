e7b8f14ec854f88e11e01980f36f4dad517cc983
lubaogui <lbaogui@pku.edu.cn>
2016-11-26 22:10:33 +0800

    update

--- alice_common/build/alice_description/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;nodelet;message_runtime;std_msgs;sensor_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lalice_description".split(';') if "-lalice_description" != "" else []
-PROJECT_NAME = "alice_description"
-PROJECT_SPACE_DIR = "/home/caros/AliceRobot/alice_common/install"
-PROJECT_VERSION = "0.0.1"
